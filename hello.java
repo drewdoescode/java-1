3aad2519e3d63b6537279da85be63b7f747567d5
Drew Gibbs <drewdoesgamemaker@gmail.com>
2015-09-04 18:44:07 -0400

    save

---
+++ hello.java
@@ -0,0 +1,11 @@
+
+import skilstak.c;
+
+public class hello {
+
+    public static void main(String[] args) {
+
+        System.out.println(c.clear + c.yellow + "HELLO" + c.violet + " W" + c.red + "O" + c.green + "R" + c.blue + "K" + c.x);
+    }
+
+}
---
+++ hi.java
@@ -0,0 +1,23 @@
+
+import skilstak.c;
+import java.util.Scanner;
+
+
+
+
+
+public class hi {
+
+    public static void main(String[] args) {
+
+        System.out.println(c.clear + c.red + "Hi WHATS YUR NAME?????" + c.x);
+        System.out.print(c.y + "> " + c.v);
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println(c.b + "WELL " + c.r + name + c.g + " nice to meet u");
+    
+    
+    
+    }
+
+}
