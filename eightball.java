c76e86d4e40c213d3777be835eed906ace2b8a7c
Drew Gibbs <drewdoesgamemaker@gmail.com>
2015-09-12 16:58:00 -0400

    save

---
+++ eightball.java
@@ -0,0 +1,40 @@
+
+import skilstak.c;
+import java.util.Scanner;
+import java.util.List;
+
+
+public class eightball {
+    private static String[] list = {"Yes.","No.","Maybe."};    
+    
+     
+    
+    private static final String input(String prompt) {
+        System.out.print(prompt);
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        return text;
+    
+    
+    
+    
+    }
+    private static final String choice(String[] list) {
+        int index = (int) (Math.random()*list.length);
+        return list[index];
+    }
+
+    public static final void main (String[] args) {
+        System.out.println(c.r + "Welcome " + c.o + "to " + c.y + "the " + c.g + "magic " + c.b + "eightball" + c.v + c.x); 
+        System.out.println(c.y + "Enter yur question below");
+        while (true) {
+            String question = input(c.r + "> " + c.b3);        
+            System.out.println(choice(list));
+
+
+
+
+
+        }
+    }
+}
--- hi.java
+++ hi.java
@@ -10,7 +10,7 @@ public class hi {
 
     public static void main(String[] args) {
 
-        System.out.println(c.clear + c.red + "Hi WHATS YUR NAME?????" + c.x);
+        System.out.println(c.clear + c.red + "H" + c.y + "i" + c.o + " W" + c.g + "H" + c.b + "A" + c.m + "T" + c.r + "S " + c.o + "YUR NAME?????" + c.x);
         System.out.print(c.y + "> " + c.v);
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
