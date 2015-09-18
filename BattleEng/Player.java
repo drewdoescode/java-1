b2af0f8829182f8f3483864cec8f526a0d4cfff3
Drew Gibbs <drewdoesgamemaker@gmail.com>
2015-09-18 18:54:48 -0400

    save

---
+++ BattleEng/Player.java
@@ -0,0 +1,9 @@
+
+public final class Player extends Entity {
+
+
+
+
+
+
+}
---
+++ BattleEng/WindowsXP.java
@@ -0,0 +1,8 @@
+final class WindowsXP extends Creature {
+     public String name = "WindowsXP";
+
+
+    public String getName() {
+        return this.name;
+    }
+}
