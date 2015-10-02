6a912672069b734e3c101673dae039d1893905c0
Drew Gibbs <drewdoesgamemaker@gmail.com>
2015-10-02 18:38:19 -0400

    save

---
+++ SkilMon/src/skilstak/skilmon/SkilMon.java
@@ -0,0 +1,20 @@
+package skilstak.skilmon;
+
+import java.util.ArrayList;
+public final class SkilMon {
+    
+    public int attack, defense, exp, happiness, hp, spAttack, spDefense, speed, total, evolvesAt;
+
+    public String species;
+    
+    public double cathRate, growthRate, height, width, mfRatio;
+
+
+    public ArrayList<String> Abilities, Moves, Types;
+
+
+
+
+}
+
+
---
+++ SkilMon/src/test/TestSkilMon.java
@@ -0,0 +1,16 @@
+package test;
+import skilstak.c;
+import skilstak.skilmon.SkilMon;
+
+public class TestSkilMon {
+
+    public static void main(String[] args) {
+        SkilMon s = new SkilMon();
+        System.out.println(c.rc() + "Speed:          " + s.speed);
+        System.out.println(c.rc() + "Attack:         " + s.attack);
+        System.out.println(c.rc() + "Special Attack: " + s.spAttack);
+        System.out.println(c.rc() + "Special Defense:" + s.spDefense);
+        System.out.println(c.rc() + "Happiness:       " + s.happiness);
+
+    }
+}
